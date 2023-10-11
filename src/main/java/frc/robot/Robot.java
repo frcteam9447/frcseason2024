4e0afd3ce134d666d9eb01cad67957dc31fcf859
frcteam9447 <147651170+frcteam9447@users.noreply.github.com>
2023-10-11 21:13:27 +0300

    Add files via upload

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,81 @@
+
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+
+
+public class Robot extends TimedRobot {
+
+  private final WPI_VictorSPX motor1 = new WPI_VictorSPX(1);
+  private final WPI_VictorSPX motor2 = new WPI_VictorSPX(2);
+  private final WPI_VictorSPX motor3 = new WPI_VictorSPX(3);
+  private final WPI_VictorSPX motor4 = new WPI_VictorSPX(4);
+
+  private final Joystick joy1 = new Joystick(0);
+
+
+
+  String resultColor;
+
+  private double startTime;
+
+  @Override
+  public void robotInit() {
+  }
+
+
+
+  @Override
+  public void autonomousInit() {
+    startTime = Timer.getFPGATimestamp();
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+    double time = Timer.getFPGATimestamp();
+    System.out.println(time - startTime);
+
+    if (time - startTime < 3) {
+      motor1.set(0.6);
+      motor2.set(0.6);
+      motor3.set(-0.6);
+      motor4.set(-0.6);
+    } else {
+      motor1.set(0);
+      motor2.set(0);
+      motor3.set(0);
+      motor4.set(0);
+    }
+  }
+
+
+  @Override
+  public void teleopInit() {
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    double speed = -joy1.getRawAxis(1) * 0.6;
+    double turn = joy1.getRawAxis(4) * 0.3;
+
+    double left = speed + turn;
+    double right = speed - turn;
+
+    motor1.set(left);
+    motor2.set(left);
+    motor3.set(-right);
+    motor4.set(-right);
+  }
+
+  @Override
+  public void testInit() {
+  }
+
+  @Override
+  public void testPeriodic() {
+  }
+
+}
